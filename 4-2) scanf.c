7f88d651fadc398b80752dd5fc089be0e2f5f118
chawoojin0425 <163067232+chawoojin0425@users.noreply.github.com>
2025-08-01 09:37:11 +0900

    Create 4-2) scanf.c

---
+++ 4-2) scanf.c
@@ -0,0 +1,12 @@
+/* 섭씨온도를 화씨로 바꾸기 */
+
+#include <stdio.h>
+
+int main(){
+	double celsius;
+	printf("섭씨 온도를 화씨 온도로 바꿔주는 프로그램입니다. \n");
+	printf("섭씨 온도를 입력해 주세요: ");
+	scanf("%lf", &celsius);
+	
+	printf("섭씨%f 도는 화씨로%f 도 입니다. \n", celsius,9*celsius/5+32); 
+} 
