6e02246ffbc173e0dbcd6fa9b6ae00c9c7e0ae0a
chawoojin0425 <163067232+chawoojin0425@users.noreply.github.com>
2025-08-04 14:58:26 +0900

    Create 5-01) if.c

---
+++ 5-01) if.c
@@ -0,0 +1,15 @@
+/* if 문이란?  */
+
+#include <stdio.h>
+
+int main(){
+	int i;
+	printf("insert a number: ");
+	scanf("%d", &i);
+	
+	if(i == 7){
+		printf("You insert number 7.\n");
+	}
+	
+	return 0;
+}
