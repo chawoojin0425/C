59c670352040cc759ee6cdc07b8d54e554b3204c
chawoojin0425 <163067232+chawoojin0425@users.noreply.github.com>
2025-08-05 09:19:15 +0900

    Create 5-05) Size_comparison.c

---
+++ 5-05) Size_comparison.c
@@ -0,0 +1,31 @@
+/* 크기 비교하기  */
+
+#include <stdio.h>
+
+int main(){
+	int i,j;
+	
+	printf("크기를 비교할 두 수를 입력: ");
+	scanf("%d %d", &i, &j);
+	
+	if(i > j){
+		printf("%d 는 %d 보다 큽니다. \n",i, j);
+	}
+	if(i < j){
+		printf("%d 는 %d 보다 작습니다. \n", i, j);
+	}
+	if(i >= j){
+		printf("%d 는 %d 보다 크거나 같습니다. \n", i, j);
+	}
+	if(i <= j){
+		printf("%d 는 %d 보다 작거나 같습니다. \n", i, j);
+	}
+	if(i == j){
+		printf("%d 는 %d 와(과) 같습니다. \n", i, j);
+	}
+	if(i != j){
+		printf("%d 는 %d 와(과) 다릅니다. \n", i, j);
+	}
+	
+	return 0;
+}
