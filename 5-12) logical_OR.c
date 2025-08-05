0e2fc7b3d718f2f2ae4fb96b4731ee0e2bd2dd45
chawoojin0425 <163067232+chawoojin0425@users.noreply.github.com>
2025-08-05 10:04:58 +0900

    Create 5-12) logical_OR.c

---
+++ 5-12) logical_OR.c
@@ -0,0 +1,15 @@
+/* 논리 합 */
+
+#include <stdio.h>
+
+int main(){
+	int height, weight;
+	printf("Insert your height and weight: ");
+	scanf("%d %d", &height, &weight);
+	
+	if(height >= 190 || weight >= 100){
+		printf("You're a giant. \n");
+	}
+	
+	return 0;
+}
